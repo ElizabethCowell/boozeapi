44b0a246a53b9b066b8585e7bb80e125b6499969
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-08-31 09:39:53 -0400

    repeat added

--- Program.cs
+++ Program.cs
@@ -10,59 +10,48 @@ namespace boozeapi
         static void Main(string[] args)
         {
             var cocktail = new HttpClient();
-
-            bool answer = false;
-            string listOption = "";
+            bool repeat = true;
             do
             {
-                Console.WriteLine("What liquor do you have on hand?");
-                var liquor = Console.ReadLine();
-                listOption = cocktail.GetStringAsync($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={liquor}").Result;
-                if (listOption != "")
-                { 
-                    answer = true;
-                }
-                else
-                {
-                    Console.WriteLine("That ingredient doesn't seem to be listed. Please try again.");
-                }
-            } while (answer == false);
-
-            var rnd = new Random();
-            var selected = false;
-            JToken drinkId = " ";
-            do
-            {
-                bool drink = false;
+                bool answer = false;
+                string listOption = "";
                 do
                 {
-                    var rndOption = rnd.Next(0, 100);
-
-                    var yourOption = JToken.Parse(listOption).SelectToken($"drinks[{rndOption}].strDrink");
-                    drinkId = JToken.Parse(listOption).SelectToken($"drinks[{rndOption}].idDrink");
-
-                    if (yourOption != null)
+                    Console.WriteLine("What liquor do you have on hand?");
+                    var liquor = Console.ReadLine();
+                    listOption = cocktail.GetStringAsync($"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={liquor}").Result;
+                    if (listOption != "")
                     {
-                        Console.WriteLine($"How about a {yourOption}? \n Is this what you want? Y/N?");
-                        drink = true;
+                        answer = true;
                     }
-                } while (drink == false);
+                    else
+                    {
+                        Console.WriteLine("That ingredient doesn't seem to be listed. Please try again.");
+                    }
+                } while (answer == false);
+
+                var rnd = new Random();
+                var selected = false;
+                JToken drinkId = " ";
+                do
+                {
+                    bool drink = false;
+                    do
+                    {
+                        var rndOption = rnd.Next(0, 100);
+
+                        var yourOption = JToken.Parse(listOption).SelectToken($"drinks[{rndOption}].strDrink");
+                        drinkId = JToken.Parse(listOption).SelectToken($"drinks[{rndOption}].idDrink");
+
+                        if (yourOption != null)
+                        {
+                            Console.WriteLine($"How about a {yourOption}? \n Is this what you want? Y/N?");
+                            drink = true;
+                        }
+                    } while (drink == false);
 
 
-                var response = Console.ReadLine().ToLower();
-                if (response == "n")
-                {
-                    selected = false;
-                }
-                else if (response == "y")
-                {
-                    selected = true;
-                }
-                else
-                {
-                    Console.WriteLine("I'm sorry, that response is invalid. Please select Y/N");
-                    response = Console.ReadLine().ToLower();
-
+                    var response = Console.ReadLine().ToLower();
                     if (response == "n")
                     {
                         selected = false;
@@ -71,31 +60,53 @@ namespace boozeapi
                     {
                         selected = true;
                     }
-                }
+                    else
+                    {
+                        Console.WriteLine("I'm sorry, that response is invalid. Please select Y/N");
+                        response = Console.ReadLine().ToLower();
 
-            } while (selected == false);
+                        if (response == "n")
+                        {
+                            selected = false;
+                        }
+                        else if (response == "y")
+                        {
+                            selected = true;
+                        }
+                    }
 
-            var ingredients = cocktail.GetStringAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drinkId}").Result;
+                } while (selected == false);
 
-            JToken item = " ";
-            JToken measurement = "";
-            int i = 1;
-            do
+                var ingredients = cocktail.GetStringAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drinkId}").Result;
 
-            {
-                item = JToken.Parse(ingredients).SelectToken($"drinks[0].strIngredient{i}");
-                measurement = JToken.Parse(ingredients).SelectToken($"drinks[0].strMeasure{i}");
-                if (item != null)
+                JToken item = " ";
+                JToken measurement = "";
+                int i = 1;
+                do
+
                 {
-                    Console.WriteLine($"{item} {measurement}");
+                    item = JToken.Parse(ingredients).SelectToken($"drinks[0].strIngredient{i}");
+                    measurement = JToken.Parse(ingredients).SelectToken($"drinks[0].strMeasure{i}");
+                    if (item != null)
+                    {
+                        Console.WriteLine($"{item} {measurement}");
+                    }
+                    i++;
+
+
+                } while (i <= 12);
+
+                var directions = JToken.Parse(ingredients).SelectToken("drinks[0].strInstructions");
+                Console.WriteLine(directions);
+
+                Console.WriteLine("Would you like another suggestion? Y/N?");
+                var again = Console.ReadLine();
+                if (again == "y")
+                {
+                    repeat = false;
                 }
-                i++;
-
-
-            } while (i <= 12);
-
-            var directions = JToken.Parse(ingredients).SelectToken("drinks[0].strInstructions");
-            Console.WriteLine(directions);
+            }
+            while (repeat == true);
         }
     }
 }
